f846f6deaddc038bca744e38a06d471f4f585035
raziel <Raziel@codefresh.net>
2017-08-22 15:56:26 -0700

    web service

--- slackbot.go
+++ slackbot.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main() {
-	fmt.Printf("Hello, world.\n")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
