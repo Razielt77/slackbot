7dce7d1602935090c00486fa87bfe605ebd6d09a
Raziel Tabib <raziel@MacBook-Pro-3.local>
2019-02-08 15:00:38 -0800

    cleaning

--- slackactions.go
+++ slackactions.go
@@ -105,81 +105,12 @@ func (r *slackActionMsg) DialogSubmission () bool {
 	return true
 }
 
-type cftokenDialogElement struct {
-	Label			string `json:"label"`
-	Name			string `json:"name"`
-	Type 			string `json:"type"`
-	Placeholder 	string `json:"placeholder"`
-}
 
-
-type cftokenDialog struct {
-	CallbackID		string `json:"callback_id"`
-	Title			string `json:"title"`
-	SubmitLabel 	string `json:"submit_label"`
-	Elements 		[]cftokenDialogElement `json:"elements"`
-}
-
-type cftokenDialogMsg struct {
-	TriggerID		string `json:"trigger_id"`
-	Dialog			cftokenDialog `json:"dialog"`
-}
 
 func (r *slackActionMsg) AskToken () bool {
 
 
 	fmt.Printf("Executing add-token action\n")
-
-	/*var tknDlg cftokenDialogMsg
-	tknDlg.TriggerID = r.TriggerID
-	tknDlg.Dialog.CallbackID = r.CallbackId
-	tknDlg.Dialog.Title = "Your Codefresh Token"
-	tknDlg.Dialog.Elements = []cftokenDialogElement{{Name: "cftoken", Label: "Codefresh Token", Type: "text",Placeholder: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}}
-
-	bearer := "Bearer " + access_token
-	url := "https://slack.com/api/dialog.open"
-
-
-	bt, err := json.Marshal(tknDlg)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-
-
-
-
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bt))
-	req.Header.Set("Authorization", bearer)
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-
-	fmt.Printf("Sending Dialog Json: %s\n", string(bt))
-
-	fmt.Printf("Sending to URL: %s, Header: %v\n", req.URL, req.Header)
-
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err ==nil {
-			bodyString := string(bodyBytes)
-			fmt.Printf("Received Response: Status: %s, Body: %s\n", resp.Status, bodyString)
-		}else{
-			fmt.Println("error:", err)
-		}
-	}else{
-		fmt.Printf("Received Response: Status: %s\n", resp.Status)
-	}*/
-
 
 
 	/*type Dialog struct {
