648618e3e62b88f4a794d1095aa5481d933b8f61
Raziel Tabib <raziel@MacBook-Pro-3.local>
2019-02-19 11:12:22 -0800

    trying to debug

--- pipelines.go
+++ pipelines.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/Razielt77/cf-webapi-go"
 	"github.com/nlopes/slack"
+	"strconv"
+	"time"
 )
 
 func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
@@ -16,19 +18,32 @@ func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
 			Footer:"Last Executed",
 			Color:"#11b5a4"}
 
-		/*if pipeline.LastWorkflow.CreatedTS != webapi.NO_LAST_WORKFLOW {
+		if pipeline.LastWorkflow.Status != webapi.NO_LAST_WORKFLOW {
+
+
+			str, err := json.Marshal(pipeline.LastWorkflow)
+
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			fmt.Printf("Pipeline has last workflow: %s\n", str)
+
 			t_start, err := time.Parse(time.RFC3339, pipeline.LastWorkflow.CreatedTS)
 			if err != nil {
-				panic(err)
+				fmt.Println(err)
 			}
 
 			t_finish, err := time.Parse(time.RFC3339, pipeline.LastWorkflow.FinishedTS)
 			if err != nil {
-				panic(err)
+				fmt.Println(err)
 			}
 			duration_t := t_finish.Sub(t_start)
 			duration := strconv.Itoa(int (duration_t.Minutes())) + " minutes."
-			p_att.Ts = json.Number(t_finish.Unix())
+
+			fmt.Printf("Duration is: %s\n", duration)
+
+			//p_att.Ts = json.Number(t_finish.Unix())
 			switch pipeline.LastWorkflow.Status{
 			case "success":
 				p_att.FooterIcon = `https://raw.githubusercontent.com/Razielt77/slackbot/master/img/passed.png`
@@ -40,13 +55,13 @@ func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
 			}
 
 
-			p_att.Fields = append(p_att.Fields,
+			/*p_att.Fields = append(p_att.Fields,
 				slack.AttachmentField{Title:"Last Status", Value:pipeline.LastWorkflow.Status, Short:true},
 				slack.AttachmentField{Title:"Duration", Value: duration , Short:true},
-				slack.AttachmentField{Title:"Last Commit", Value:pipeline.LastWorkflow.CommitMsg, Short:false})
-			p_att.AuthorIcon = pipeline.LastWorkflow.Avatar
-			p_att.AuthorName= pipeline.LastWorkflow.Committer
-		}*/
+				slack.AttachmentField{Title:"Last Commit", Value:pipeline.LastWorkflow.CommitMsg, Short:false})*/
+			//p_att.AuthorIcon = pipeline.LastWorkflow.Avatar
+			//p_att.AuthorName= pipeline.LastWorkflow.Committer
+		}
 
 		str, err := json.Marshal(p_att)
 
