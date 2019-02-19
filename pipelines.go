23b84d457e4550695f45b6cc28395f9883a28a03
Raziel Tabib <raziel@MacBook-Pro-3.local>
2019-02-19 12:14:55 -0800

    trying to debug

--- pipelines.go
+++ pipelines.go
@@ -15,7 +15,6 @@ func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
 		p_att := slack.Attachment{
 			Title:pipeline.Name,
 			TitleLink:string(`https://g.codefresh.io/pipelines/edit/summary?id=` + pipeline.ID),
-			Footer:"Last Executed",
 			Color:"#11b5a4"}
 
 		if pipeline.LastWorkflow.Status != webapi.NO_LAST_WORKFLOW {
@@ -54,11 +53,13 @@ func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
 				p_att.Color="#ccc"
 			}
 
+			p_att.Footer = "Last Executed: " + "<!date^" + strconv.FormatInt(t_finish.Unix(),10) + "^{date} at {time}|Not Set>"
 
+			commit := "<" + pipeline.LastWorkflow.CommitUrl + "|" + pipeline.LastWorkflow.CommitMsg + ">"
 			p_att.Fields = append(p_att.Fields,
 				slack.AttachmentField{Title:"Last Status", Value:pipeline.LastWorkflow.Status, Short:true},
 				slack.AttachmentField{Title:"Duration", Value: duration , Short:true},
-				slack.AttachmentField{Title:"Last Commit", Value:pipeline.LastWorkflow.CommitMsg, Short:false})
+				slack.AttachmentField{Title:"Last Commit", Value:commit, Short:false})
 			p_att.AuthorIcon = pipeline.LastWorkflow.Avatar
 			p_att.AuthorName= pipeline.LastWorkflow.Committer
 		}
