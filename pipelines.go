84a778b1d307085c5950068d013c26780b1da252
Raziel Tabib <raziel@macbook-pro-3.attlocal.net>
2019-03-01 22:43:41 -0800

    changing active to 3 days

--- pipelines.go
+++ pipelines.go
@@ -68,6 +68,7 @@ func ComposePipelinesAtt(p_arr []webapi.Pipeline) []slack.Attachment {
 			p_att.Footer = "Last Executed: " + "<!date^" + strconv.FormatInt(t_finish.Unix(),10) + "^{date} at {time}|Not Set>"
 
 			commit := "<" + pipeline.LastWorkflow.CommitUrl + "|" + pipeline.LastWorkflow.CommitMsg + ">"
+			commit = strings.Replace(commit,"\n"," ",-1)
 			p_att.Fields = append(p_att.Fields,
 				slack.AttachmentField{Title:"Last Status", Value:pipeline.LastWorkflow.Status, Short:true},
 				slack.AttachmentField{Title:"Duration", Value: duration , Short:true},
