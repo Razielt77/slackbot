52cd7762927f01900fdd6f5f714ebb348a1d567e
Raziel Tabib <raziel@MacBook-Pro-3.local>
2019-03-08 13:46:43 -0800

    with right token

--- slackbot.go
+++ slackbot.go
@@ -112,7 +112,7 @@ func HandleEvent (s *mgo.Session) func(w http.ResponseWriter, r *http.Request){
 
 		fmt.Printf("Body: %s\n",body)
 
-		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: access_token}))
+		eventsAPIEvent, _ := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: access_token}))
 		//eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body))
 
 		/*if e != nil {
